9c42850e768389ce0efc38879c319a8bc97a245e
Emrah Atalay <emrahatalay92@gmail.com>
2024-01-17 22:06:13 +0300

    Add project files.

---
+++ Src/TCMB.UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TCMB.UnitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
